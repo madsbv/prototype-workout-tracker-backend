fce3d39af68465ab5d482e2f499e827c337f22ce
madsbv <mbvilladsen@pm.me>
2024-02-20 17:20:34 +0100

    Switch time parsing to humantime_serde crate

--- src/strong_data.rs
+++ src/strong_data.rs
@@ -1,8 +1,5 @@
 use serde::{Deserialize, Serialize};
-use serde_with::{DeserializeFromStr, SerializeDisplay};
-use std::fmt;
-use std::str::FromStr;
-use time::PrimitiveDateTime;
+use std::time::{Duration, SystemTime};
 
 pub fn parse_strong_csv_to_exercise_data(path: &str) -> anyhow::Result<Vec<StrongData>> {
     let mut strong_rdr = csv::Reader::from_reader(std::fs::File::open(path)?);
@@ -13,104 +10,17 @@ pub fn parse_strong_csv_to_exercise_data(path: &str) -> anyhow::Result<Vec<Stron
         .collect()
 }
 
-// Use serde_with's derives to implement serialization and deserialization with the Display and FromStr traits.
-#[derive(SerializeDisplay, DeserializeFromStr, Debug, PartialEq, PartialOrd)]
-pub struct StrongDuration {
-    pub hours: u32,
-    pub minutes: u32,
-    pub seconds: u32,
-}
-
-impl fmt::Display for StrongDuration {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut parts: Vec<String> = vec![];
-        if self.hours != 0 {
-            parts.push(format!("{}h", self.hours));
-        }
-        if self.minutes != 0 {
-            parts.push(format!("{}m", self.minutes));
-        }
-        parts.push(format!("{}s", self.seconds));
-        let result = parts.join(" ");
-        write!(f, "{result}")
-    }
-}
-
-impl FromStr for StrongDuration {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts = s.split(' ').collect::<Vec<_>>();
-        let mut hours = 0;
-        let mut minutes = 0;
-        let mut seconds = 0;
-        match parts.len() {
-            3 => {
-                hours = parse_suffixed_integer(parts[0], 'h')?;
-                minutes = parse_suffixed_integer(parts[1], 'm')?;
-                seconds = parse_suffixed_integer(parts[2], 's')?;
-            }
-            2 => {
-                hours = parse_suffixed_integer(parts[0], 'h')?;
-                minutes = parse_suffixed_integer(parts[1], 'm')?;
-            }
-            1 => {
-                if let Ok(s) = parse_suffixed_integer(parts[0], 's') {
-                    seconds = s;
-                } else {
-                    minutes = parse_suffixed_integer(parts[0], 'm')?;
-                }
-            }
-            _ => {
-                return Err(format!(
-                    "The following duration specifier has {} parts, should have 1 or 2.\n {s}",
-                    parts.len()
-                ));
-            }
-        };
-        if seconds >= 60 {
-            let extra_minutes = seconds / 60;
-            minutes += extra_minutes;
-            seconds -= extra_minutes * 60;
-        }
-        if minutes >= 60 {
-            let extra_hours = minutes / 60;
-            hours += extra_hours;
-            minutes -= extra_hours * 60;
-        }
-        Ok(StrongDuration {
-            hours,
-            minutes,
-            seconds,
-        })
-    }
-}
-
-fn parse_suffixed_integer(s: &str, suffix: char) -> Result<u32, <StrongDuration as FromStr>::Err> {
-    let chars = s.chars().collect::<Vec<_>>();
-    if chars[chars.len() - 1] != suffix {
-        return Err(format!("Invalid suffix, should be {suffix}"));
-    }
-    s[0..chars.len() - 1]
-        .parse()
-        .map_err(|_| format!("Invalid number before suffix {suffix}"))
-}
-
-time::serde::format_description!(
-    strong_date,
-    PrimitiveDateTime,
-    "[year]-[month]-[day] [hour]:[minute]:[second]"
-);
-
+// Relies on humantime_serde to serialize and deserialize timestamps and durations written in human-readable format
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct StrongData {
-    #[serde(with = "strong_date")]
-    pub date: PrimitiveDateTime,
+    #[serde(with = "humantime_serde")]
+    pub date: SystemTime,
     // pub date: String,
     #[serde(rename = "Workout Name")]
     pub workout_name: String,
-    pub duration: StrongDuration,
+    #[serde(with = "humantime_serde")]
+    pub duration: Duration,
     // pub duration: String,
     #[serde(rename = "Exercise Name")]
     pub exercise_name: String,
@@ -130,38 +40,27 @@ pub struct StrongData {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use humantime;
 
     #[test]
-    fn test_strong_duration_parsing_and_display() {
+    fn test_duration_parsing_and_display() {
         let string = "10h 30m";
-        let duration = StrongDuration {
-            hours: 10,
-            minutes: 30,
-            seconds: 0,
-        };
-        let duration_from_string = StrongDuration::from_str(string).expect("String parses");
+        let duration = Duration::new(3600 * 10 + 60 * 30, 0);
+        let duration_from_string = humantime::parse_duration(string).expect("String parses");
         assert_eq!(duration, duration_from_string);
-        assert_eq!("10h 30m 0s", duration.to_string());
+        assert_eq!("10h 30m", humantime::format_duration(duration).to_string());
 
         let string = "15m";
-        let duration = StrongDuration {
-            hours: 0,
-            minutes: 15,
-            seconds: 0,
-        };
-        let duration_from_string = StrongDuration::from_str(string).expect("String parses");
+        let duration = Duration::new(60 * 15, 0);
+        let duration_from_string = humantime::parse_duration(string).expect("String parses");
         assert_eq!(duration, duration_from_string);
-        assert_eq!("15m 0s", duration.to_string());
+        assert_eq!("15m", humantime::format_duration(duration).to_string());
 
         let string = "26s";
-        let duration = StrongDuration {
-            hours: 0,
-            minutes: 0,
-            seconds: 26,
-        };
-        let duration_from_string = StrongDuration::from_str(string).expect("String parses");
+        let duration = Duration::new(26, 0);
+        let duration_from_string = humantime::parse_duration(string).expect("String parses");
         assert_eq!(duration, duration_from_string);
-        assert_eq!("26s", duration.to_string());
+        assert_eq!("26s", humantime::format_duration(duration).to_string());
     }
 
     #[test]
@@ -179,13 +78,10 @@ Date,Workout Name,Duration,Exercise Name,Set Order,Weight,Reps,Distance,Seconds,
         let test_none_case = deserializer.next();
 
         let data = StrongData {
-            date: time::macros::datetime!(2023-07-14 15:30:00),
+            date: humantime::parse_rfc3339_weak("2023-07-14 15:30:00")
+                .expect("this time format is valid"),
             workout_name: String::from("Morning Workout "),
-            duration: StrongDuration {
-                hours: 0,
-                minutes: 30,
-                seconds: 0,
-            },
+            duration: Duration::new(30 * 60, 0),
             exercise_name: String::from("Incline Bench Press (Barbell)"),
             set_order: 1,
             weight: 29.483_504_05,
